8fd00380a0131f8d8c31c3b69c773dc0e9b06ed1
leahfortier <leahfortier318@gmail.com>
2016-11-21 08:59:03 -0800

    no more wild encounter description string
    just using the json for the WildEncounter class now

--- src/pattern/map/WildBattleMatcher.java
+++ src/pattern/map/WildBattleMatcher.java
@@ -1,36 +1,21 @@
 package pattern.map;
 
-import main.Global;
 import map.EncounterRate;
 import map.WildEncounter;
 import mapMaker.model.TriggerModel.TriggerModelType;
-import pattern.MatchConstants;
-import pattern.MatchConstants.MatchType;
 import pattern.generic.MultiPointTriggerMatcher;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class WildBattleMatcher extends MultiPointTriggerMatcher {
-    private static final Pattern wildEncounterPattern = Pattern.compile(
-            MatchConstants.group(MatchType.POKEMON_NAME) + " " +
-                    MatchConstants.group(MatchType.INTEGER) + "-" + MatchConstants.group(MatchType.INTEGER) + " " +
-                    MatchConstants.group(MatchType.INTEGER) + "%"
-    );
-
     private String name;
     private EncounterRate encounterRate;
-    private String[] pokemon;
+    private WildEncounter[] wildPokemon;
 
     public WildBattleMatcher(String name, EncounterRate encounterRate, List<WildEncounter> wildEncounters) {
         this.name = name;
         this.encounterRate = encounterRate;
-        this.pokemon = new String[wildEncounters.size()];
-        for (int i = 0; i < pokemon.length; i++) {
-            WildEncounter wildEncounter = wildEncounters.get(i);
-            this.pokemon[i] = wildEncounter.getPokemonName() + " " + wildEncounter.getMinLevel() + "-" + wildEncounter.getMaxLevel() + " " + wildEncounter.getProbability() + "%";
-        }
+        this.wildPokemon = wildEncounters.toArray(new WildEncounter[0]);
     }
 
     public EncounterRate getEncounterRate() {
@@ -38,22 +23,7 @@ public class WildBattleMatcher extends MultiPointTriggerMatcher {
     }
 
     public WildEncounter[] getWildEncounters() {
-        WildEncounter[] wildEncounters = new WildEncounter[pokemon.length];
-        for (int i = 0; i < pokemon.length; i++) {
-            Matcher matcher = wildEncounterPattern.matcher(pokemon[i]);
-            if (!matcher.matches()) {
-                Global.error("Invalid wild pokemon encounter description " + pokemon[i]);
-            }
-
-            wildEncounters[i] = new WildEncounter(
-                    matcher.group(1),   // Pokemon name
-                    matcher.group(2),   // Min level
-                    matcher.group(3),   // Max level
-                    matcher.group(4)    // Percentage probability
-            );
-        }
-
-        return wildEncounters;
+        return this.wildPokemon;
     }
 
     @Override
