23c49932748f6cb35b3d22146a8ecc2c36d17c80
leahfortier <leahfortier318@gmail.com>
2016-11-29 09:38:51 -0800

    AHHHH OKAY FINE THEY'RE ALPHABETICAL NOW HAPPY
    FUCK YOU

--- src/pattern/generic/LocationTriggerMatcher.java
+++ src/pattern/generic/LocationTriggerMatcher.java
@@ -3,11 +3,16 @@ package pattern.generic;
 import mapMaker.model.TriggerModel.TriggerModelType;
 import util.Point;
 
-public abstract class LocationTriggerMatcher extends TriggerMatcher {
+public abstract class LocationTriggerMatcher extends TriggerMatcher implements Comparable<LocationTriggerMatcher> {
     public abstract boolean isAtLocation(Point location);
     public abstract void setLocation(LocationTriggerMatcher oldMatcher);
     public abstract void addPoint(Point point);
     public abstract void addDelta(Point delta);
     public abstract TriggerModelType getTriggerModelType();
     public abstract String getBasicName();
+
+    @Override
+    public int compareTo(LocationTriggerMatcher other) {
+        return this.getTriggerName().compareTo(other.getTriggerName());
+    }
 }
