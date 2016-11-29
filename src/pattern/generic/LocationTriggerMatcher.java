b4a6c3bce3eed9b6a2449a64f85fb3261900107f
leahfortier <leahfortier318@gmail.com>
2016-11-29 13:13:32 -0800

    compare by location as well

--- src/pattern/generic/LocationTriggerMatcher.java
+++ src/pattern/generic/LocationTriggerMatcher.java
@@ -3,11 +3,34 @@ package pattern.generic;
 import mapMaker.model.TriggerModel.TriggerModelType;
 import util.Point;
 
+import java.util.Comparator;
+
 public abstract class LocationTriggerMatcher extends TriggerMatcher {
+
+    // Compare first by basic name, then by location
+    public static final Comparator<LocationTriggerMatcher> COMPARATOR = (first, second) -> {
+        int nameCompare = first.getBasicName().compareTo(second.getBasicName());
+        if (nameCompare != 0) {
+            return nameCompare;
+        }
+
+        Point firstLocation = first.getFirstLocationPoint();
+        Point secondLocation = second.getFirstLocationPoint();
+
+        int xCompare = firstLocation.x - secondLocation.x;
+        if (xCompare != 0) {
+            return xCompare;
+        }
+
+        return firstLocation.y - secondLocation.y;
+    };
+
     public abstract boolean isAtLocation(Point location);
     public abstract void setLocation(LocationTriggerMatcher oldMatcher);
     public abstract void addPoint(Point point);
     public abstract void addDelta(Point delta);
     public abstract TriggerModelType getTriggerModelType();
     public abstract String getBasicName();
+
+    protected abstract Point getFirstLocationPoint();
 }
