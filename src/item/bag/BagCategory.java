3e025335e0344e126a0e9d5acfe9394b1461ab78
leahfortier <leahfortier318@gmail.com>
2016-12-06 15:27:54 -0800

    move bag tiles

--- src/item/bag/BagCategory.java
+++ src/item/bag/BagCategory.java
@@ -1,37 +1,42 @@
 package item.bag;
 
+import util.FileIO;
+import util.Folder;
+
 import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.Serializable;
 
 public enum BagCategory implements Serializable {
-    MEDICINE("Medicine", new Color(248, 120, 64), 0x1),
-    BALL("Balls", new Color(232, 184, 40), 0x2),
-    STAT("Stat", new Color(80, 128, 232), 0x3),
-    KEY_ITEM("KeyItems", new Color(152, 88, 240), 0x4),
-    TM("TMs", new Color(168, 232, 72), 0x5),
-    BERRY("Berries", new Color(64, 192, 64), 0x6),
-    MISC("Misc", new Color(232, 136, 192), 0x7);
+    MEDICINE("Medicine", new Color(248, 120, 64)),
+    BALL("Balls", new Color(232, 184, 40)),
+    STAT("Stat", new Color(80, 128, 232)),
+    KEY_ITEM("Key Items", new Color(152, 88, 240)),
+    TM("TMs", new Color(168, 232, 72)),
+    BERRY("Berries", new Color(64, 192, 64)),
+    MISC("Misc", new Color(232, 136, 192));
 
-    // Name to display in BagView
-    private String name;
-    private Color color;
-    private int imageNumber;
+    private final String displayName;
+    private final Color color;
+    private final BufferedImage icon;
 
-    BagCategory(String name, Color color, int imageNumber) {
-        this.name = name;
+    BagCategory(String displayName, Color color) {
+        this.displayName = displayName;
         this.color = color;
-        this.imageNumber = imageNumber;
+
+        String imageName = "cat_" + this.displayName.replaceAll("\\s", "").toLowerCase();
+        this.icon = FileIO.readImage(Folder.BAG_TILES + imageName);
     }
 
-    public String getName() {
-        return name;
+    public String getDisplayName() {
+        return displayName;
     }
 
     public Color getColor() {
         return color;
     }
 
-    public int getImageNumber() {
-        return imageNumber;
+    public BufferedImage getIcon() {
+        return icon;
     }
 }
