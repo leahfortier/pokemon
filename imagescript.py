1b8f072febfc7be2c856ddb6f7472eee5123b93b
leahfortier <leahfortier318@gmail.com>
2017-02-13 18:33:52 -0800

    gen 7 small tiles :)

--- imagescript.py
+++ imagescript.py
@@ -1,6 +1,6 @@
 import urllib
 
-for num in range(1, 802):
-    source = "http://www.serebii.net/art/th/" + str(num) + ".png"
-    dest = "C:\\Users\\leahf_000\\Documents\\IdeaProjects\\Pokemon++\\rec\\images\\tiles\\pokedexTiles\\" + str(num).zfill(3) + ".png"
+for num in range(720, 802):
+    source = "http://www.serebii.net/pokedex-sm/icon/" + str(num) + ".png"
+    dest = "C:\\Users\\leahf_000\\Documents\\IdeaProjects\\Pokemon++\\rec\\images\\tiles\\partyTiles\\" + str(num).zfill(3) + "-small.png"
     urllib.urlretrieve(source, dest)
