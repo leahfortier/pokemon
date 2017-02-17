3b8ebff940914a362d76cbe22d6b573b25532d94
leahfortier <leahfortier318@gmail.com>
2017-02-17 15:55:57 -0800

    fix

--- src/map/overworld/WalkType.java
+++ src/map/overworld/WalkType.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public enum WalkType {
     NOT_WALKABLE(0x000000, false),
     WALKABLE(0xFFFFFF, true),
-    WATER(0x0000FF, Game.getPlayer().hasTool(OverworldTool.SURF)),
+    WATER(0x0000FF, direction -> Game.getPlayer().hasTool(OverworldTool.SURF)),
     DOWN_LEDGE(0x00FF00, Direction.DOWN),
     UP_LEDGE(0xFF0000, Direction.UP),
     LEFT_LEDGE(0xFFFF00, Direction.LEFT),
