b5f021711987a5285e75b3fc4f719428471787ba
leahfortier <leahfortier318@gmail.com>
2017-03-15 11:28:11 -0700

    Mimikyu's holy mantle was busted!

--- src/pokemon/ability/Ability.java
+++ src/pokemon/ability/Ability.java
@@ -3717,7 +3717,14 @@ public abstract class Ability implements Serializable, AbilityHolder {
 
 		public boolean absorbDamage(Battle b, ActivePokemon damageTaker, int damageAmount) {
 			if (!activated && b.getOtherPokemon(damageTaker).getAttributes().isAttacking()) {
-				Messages.add(damageTaker.getName() + "'s disguise was busted!!");
+				boolean isPlayer = damageTaker.isPlayer();
+				boolean shiny = damageTaker.isShiny();
+				boolean front = !isPlayer;
+				
+				Messages.add(new MessageUpdate(damageTaker.getName() + "'s disguise was busted!!")
+					.withImageName(damageTaker.getPokemonInfo().getImageName(shiny, front, true), isPlayer)
+				);
+				
 				activated = true;
 				return true;
 			}
