7162851a38b07c1382b9edf0178dae4403dbcbc8
leahfortier <leahfortier318@gmail.com>
2018-02-03 17:42:08 -0800

    condition fixes

--- src/map/condition/OrCondition.java
+++ src/map/condition/OrCondition.java
@@ -6,8 +6,11 @@ import java.util.List;
 public class OrCondition implements Condition {
     private final List<Condition> conditions;
 
-    public OrCondition() {
+    public OrCondition(Condition... conditions) {
         this.conditions = new ArrayList<>();
+        for (Condition condition : conditions) {
+            this.or(condition);
+        }
     }
 
     public OrCondition or(Condition condition) {
